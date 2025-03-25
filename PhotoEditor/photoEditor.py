1a51045ab07e6a508cef5a96a4e5f21aad040228
aezazueta <alain.apok@gmail.com>
2025-03-24 22:50:35 -0700

    se agregaron nuevos filtros, se cambió la ruta definida manualmente por una automatizada y se generó un archivo de salida por filtro

--- PhotoEditor/photoEditor.py
+++ PhotoEditor/photoEditor.py
@@ -1,12 +1,65 @@
 from PIL import Image, ImageEnhance, ImageFilter
 import os
 
-path = 'PhotoEditor/imgs'
-pathOut = 'PhotoEditor/editedImgs'
+# ruta del archivo actual
+path_file = os.path.abspath(__file__)
+# ruta de la carpeta de imagenes
+path_img = os.path.dirname(path_file)+'/imgs'
+print(path_img)
 
-for file in os.listdir(path):
+pathOut = os.path.dirname(path_file)+'/editedImgs'
+print(pathOut)
+
+print('Editando imagenes con resaltado de bordes...')
+# recorrido de la carpeta de imagenes
+for file in os.listdir(path_img):
     #carga de la imagen a editar
-    img = Image.open(f"{path}/{file}")
+    img = Image.open(f"{path_img}/{file}")
+    # aplicacion de filtro de nitidez con conversion a escala de grises (modo L)
+    edit = img.filter(ImageFilter.CONTOUR)
+
+    # separacion del nombre del archivo de su extension
+    clean_name = os.path.splitext(file)[0]
+    # conversion de la imagen de RGB
+    edit_im = edit.convert('RGB')
+    # guardado de la imagen editada en la carpeta de salida
+    edit_im.save(f'{pathOut}/{clean_name}_edit_CONTOUR.jpg')
+
+print('Editando imagenes con efecto blur...')
+# recorrido de la carpeta de imagenes
+for file in os.listdir(path_img):
+    #carga de la imagen a editar
+    img = Image.open(f"{path_img}/{file}")
+    # aplicacion de filtro de nitidez con conversion a escala de grises (modo L)
+    edit = img.filter(ImageFilter.BLUR)
+
+    # separacion del nombre del archivo de su extension
+    clean_name = os.path.splitext(file)[0]
+    # conversion de la imagen de RGB
+    edit_im = edit.convert('RGB')
+    # guardado de la imagen editada en la carpeta de salida
+    edit_im.save(f'{pathOut}/{clean_name}_edit_BLUR.jpg')
+
+print('Editando imagenes con delineado de bordes...')
+# recorrido de la carpeta de imagenes
+for file in os.listdir(path_img):
+    #carga de la imagen a editar
+    img = Image.open(f"{path_img}/{file}")
+    # aplicacion de filtro de nitidez con conversion a escala de grises (modo L)
+    edit = img.filter(ImageFilter.FIND_EDGES)
+
+    # separacion del nombre del archivo de su extension
+    clean_name = os.path.splitext(file)[0]
+    # conversion de la imagen de RGB
+    edit_im = edit.convert('RGB')
+    # guardado de la imagen editada en la carpeta de salida
+    edit_im.save(f'{pathOut}/{clean_name}_edit_FIND_EDGES.jpg')
+
+print('Editando imagenes con mayor nitidez, contraste y conversion a escala de grises...')
+# recorrido de la carpeta de imagenes
+for file in os.listdir(path_img):
+    #carga de la imagen a editar
+    img = Image.open(f"{path_img}/{file}")
     # aplicacion de filtro de nitidez con conversion a escala de grises (modo L)
     edit = img.filter(ImageFilter.SHARPEN).convert('L')
     # creacion de un valor para incrementar contraste
@@ -20,4 +73,6 @@ for file in os.listdir(path):
     # conversion de la imagen de RGB
     edit_im = edit.convert('RGB')
     # guardado de la imagen editada en la carpeta de salida
-    edit_im.save(f'{pathOut}/{clean_name}_edited.jpg')
+    edit_im.save(f'{pathOut}/{clean_name}_edit_sharpen_BW.jpg')
+
+print('Edicion de imagenes finalizada...')
